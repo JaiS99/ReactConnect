323dc224e38b1ce5394bec75f06beb8eb0274877
0Armaan025 <armaan33000@gmail.com>
2023-07-29 12:32:31 +0530

    made create-space screen

--- reactconnect/src/App.js
+++ reactconnect/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./screens/home/Home";
+import CreateSpace from "./screens/create-space/CreateSpace";
 
 function App() {
   return (
     <>
     <Routes>
       <Route exact path="/" element={<Home/>} />
+      <Route exact path="/create-space" element={<CreateSpace/>} />
    </Routes>
    </>  
   );
--- reactconnect/src/screens/create-space/CreateSpace.jsx
+++ reactconnect/src/screens/create-space/CreateSpace.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import './createspace.css'
+import NavBar from '../../components/navbar/Navbar'
 
 const CreateSpace = () => {
   return (
     <>
+
+    <div className="createSpace">
+
+        <center>
+        <NavBar/>
+        <br/>
+            <h1 className='headingText'>
+                Create Virtual Space 🤖
+            </h1>
+
+            <br/>
+
+            
+
+            <h4>
+                Please enter the following details.
+            </h4>   
+            <br/>
+            <div className="detailsNewContainer">
+
+                <input type='text' placeholder='Space Name' className='textField'/><br/>
+
+                <textarea className='textField' placeholder='Description about space.'></textarea> <br/>
+
+                <button className='create-space-btn'>
+      Create Space
+    </button>
+
+            </div>
+
+        </center>
+
+    </div>
+
     </>
   )
 }
