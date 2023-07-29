9b0c91335871bc294504461a7ce3dd7d629132e0
0Armaan025 <armaan33000@gmail.com>
2023-07-29 09:14:42 +0530

    making navbar

---
+++ reactconnect/src/screens/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.css';
+import Navbar from '../../components/navbar/Navbar';
+
+const Home = () => {
+  return (
+    <>
+    <div className="homeContainer">
+      <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default Home
